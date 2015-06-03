ececefce1ffefcc71f47aebbb83abfda66b5b893
MagoDopado <cirano_eusebi@hotmail.com>
2015-06-02 21:41:32 -0300

    Fixed article controller

--- src/app/Http/Controllers/ArticlesController.php
+++ src/app/Http/Controllers/ArticlesController.php
@@ -54,52 +54,6 @@ class ArticlesController extends Controller {
 	 */
 	public function show($id)
 	{
-		$article = Article::find($id);
-
-		if (!$article) {
-			Session::flash('error', 'No se encontró: '.$id);
-		}
-
-		return view('articles.show')
-					->with('article', $article);
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		$all = Request::all();
-
-		$validator = Validator::make($all,
-			['name' => ['required', 'min:5', 'max:50']]
-		);
-
-		if ( $validator->fails() )
-		{
-			$errors = $validator->errors()->all();
-
-			return redirect()
-				->back()
-				->with('errors', $errors);
-		}
-
-		Article::create($all);
-
-		return redirect()
-				->route('articles.index')
-				->with('success', 'Se guardó correctamente');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
 		//Find the requested article.
 		$article = Article::find($id);
 
@@ -123,38 +77,4 @@ class ArticlesController extends Controller {
 					->with('article', $article)
 					->with('related', $related);
 	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
 }
