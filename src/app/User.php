ab0ceec0fd4240c7e7911ec6784b9ec691661320
Bruno Cascio <brunocascio@gmail.com>
2015-06-04 19:46:31 -0300

    Traduction for messages

--- src/app/User.php
+++ src/app/User.php
@@ -51,21 +51,48 @@ class User extends Model implements Authenticatable {
 	*/
 
 	private static $rulesForAdmin = [
-		'firstName' => 'required|min:3|max:30',
-		'lastName' => 'required|min:3|max:30',
-		'email' => 'required|min:10|max:30|unique:users',
+		'firstName' => 'required|alpha|min:3|max:30',
+		'lastName' => 'required|alpha|min:3|max:30',
+		'email' => 'required|email|min:10|max:30|unique:users',
 		'username' => 'required|min:4|max:16|unique:users',
 		'password' => 'required|min:6',
 		'rol' => 'required|in:user,admin'
 	];
 
 	private static $rulesForCreation = [
-		'firstName' => 'required|min:3|max:30',
-		'lastName' => 'required|min:3|max:30',
-		'email' => 'required|min:10|max:30|unique:users',
+		'firstName' => 'required|alpha|min:3|max:30',
+		'lastName' => 'required|alpha|min:3|max:30',
+		'email' => 'required|email|min:10|max:30|unique:users',
 		'username' => 'required|min:4|max:16|unique:users',
-		'password' => 'required|min:6',
-		'repassword' => 'required|same:password',
+		'password' => 'required|min:6|same:repassword'
+	];
+
+	private static $messages = [
+		// firstName messages
+    'firstName.required' => 'El nombre es requerido',
+		'firstName.alpha' => 'El nombre no puede contener números ni símbolos',
+		'firstName.min' => 'El nombre debe tener al menos :min caracteres',
+		'firstName.max' => 'El nombre debe tener menos de :max caracteres',
+		// lastName messages
+		'lastName.required' => 'El apellido es requerido',
+		'lastName.alpha' => 'El apellido no puede contener números ni símbolos',
+		'lastName.min' => 'El apellido debe tener al menos :min caracteres',
+		'lastName.max' => 'El apellido debe tener menos de :max caracteres',
+		// email messages
+		'email.required' => 'El email es requerido',
+		'email.email' => 'El email no es una dirección de correo electrónica válida',
+		'email.min' => 'El email debe tener al menos :min caracteres',
+		'email.max' => 'El email debe tener menos de :max caracteres',
+		'email.unique' => 'Un usuario con este email, ya se encuentra registrado',
+		// username messages
+		'username.required' => 'El nombre de usuario es requerido',
+		'username.min' => 'El nombre de usuario debe tener al menos :min caracteres',
+		'username.max' => 'El nombre de usuario debe tener menos de :max caracteres',
+		'username.unique' => 'Un usuario con este nombre de usuario, ya se encuentra registrado',
+		// password messages
+		'password.required' => 'La contraseña es requerida',
+		'password.min' => 'La contraseña debe tener al menos 6 caracteres',
+		'password.same' => 'Las contraseñas no coinciden'
 	];
 
 	/*
@@ -78,12 +105,13 @@ class User extends Model implements Authenticatable {
 	*/
 	public static function validate($all) {
 		$rules = self::$rulesForCreation;
+		$messages = self::$messages;
 
 		if ( self::currentUserIsAdmin() ) {
 			$rules = self::$rulesForAdmin;
 		}
 
-		return Validator::make($all, $rules);
+		return Validator::make($all, $rules, $messages);
 	}
 
 	/*
