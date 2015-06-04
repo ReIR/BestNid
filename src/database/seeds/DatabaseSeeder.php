5803a938fad8b3225fbcbbbd287a8e0850cbf5d9
Bruno Cascio <brunocascio@gmail.com>
2015-06-03 22:58:00 -0300

    change description in articles

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -47,7 +47,7 @@ class DatabaseSeeder extends Seeder {
 		// 1ra forma
 		Article::create([
 			'title' => 'Mercedes Benz',
-			'description' => 'Acá está mi auto re pillo.',
+			'description' => 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
 			'image' => 'mercedes_benz.png',
 			'endDate' => (new DateTime('2015-08-18'))->format('Y-m-d H:i:s'),
 
@@ -57,7 +57,7 @@ class DatabaseSeeder extends Seeder {
 
 		Article::create([
 			'title' => 'Audi R8',
-			'description' => 'Mi auto es mucho mejor.',
+			'description' => 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
 			'image' => 'audi.png',
 			'endDate' => (new DateTime('2015-09-21'))->format('Y-m-d H:i:s'),
 
@@ -78,7 +78,7 @@ class DatabaseSeeder extends Seeder {
 
 		Article::create([
 			'title' => 'Mi falcon',
-			'description' => 'Todavia anda.',
+			'description' => 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.',
 			'image' => 'falcon.png',
 			'endDate' => (new DateTime('2015-12-24'))->format('Y-m-d H:i:s'),
 
