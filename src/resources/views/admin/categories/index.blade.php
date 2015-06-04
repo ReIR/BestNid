fcb382ca67bec77b6dda0405c77e828791ba4fe3
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-06-03 22:10:58 -0300

    Se agrega botón para crear categoría en vista categories.

--- src/resources/views/admin/categories/index.blade.php
+++ src/resources/views/admin/categories/index.blade.php
@@ -5,26 +5,30 @@
 @section('content')
 
 <div class="col-md-9">
-	<table class="table table-hover">
-		<thead>
-			<tr>
-				<th>Nombre categoría</th>
-				<th>Acciones</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach ($categories as $c)
+	<div class="row pull-right">
+		<a class="btn btn-default" href="{{route('admin.categories.create')}}" role="button">Crear categoría</a>
+	</div>
+	<div class="row">
+		<table class="table table-hover">
+			<thead>
 				<tr>
-					<td>{{$c->name}}</td>
-					<td
-						<div class="pull-right">
-							<a class="btn btn-default" href="#" role="button">Editar</a>
-							<a class="btn btn-danger" href="{{route ('admin.categories.alert', ['id'=>$c->id])}}" role="button">Borrar</a>
-						</div>
-					</td>
+					<th colspan="2">Nombre categoría</th>
 				</tr>
-			@endforeach
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				@foreach ($categories as $c)
+					<tr>
+						<td>{{$c->name}}</td>
+						<td
+							<div class="text-right">
+								<a class="btn btn-default" href="#" role="button">Editar</a>
+								<a class="btn btn-danger" href="{{route ('admin.categories.alert', ['id'=>$c->id])}}" role="button">Borrar</a>
+							</div>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
 </div>
 @stop
