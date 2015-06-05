bf9bda746adfe856ba248a2eba14c683f5dc215b
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2015-06-04 21:50:36 -0300

    Se modifica el lenguaje de mensajes de error en Categorías de inglés a español.

--- src/app/Category.php
+++ src/app/Category.php
@@ -9,25 +9,39 @@ class Category extends Model {
 	protected $fillable = ['name'];
 
 	private static $rulesForCreation = [
-			'name' => 'required|min:5|max:50|unique:categories,name'
+			'name' => 'required|alpha|min:4|max:50|unique:categories,name'
 	];
 
 	public static function validate($all) {
 		$rules = self::$rulesForCreation;
+		$messages = self::$messages;
 
-		return Validator::make($all, $rules);
+		return Validator::make($all, $rules, $messages);
 	}
 
 	public static function validateUpdate($all) {
 		$rulestemp = self::$rulesForCreation;
-		$rulestemp['name'] = 'required|min:5|max:50|unique:categories,name,'.$all['id'];
+		$messages = self::$messages;
 
-		return Validator::make($all, $rulestemp);
+		$rulestemp['name'] = 'required|alpha|min:4|max:50|unique:categories,name,'.$all['id'];
+
+		return Validator::make($all, $rulestemp, $messages);
 	}
 
 	//
 	public function articles() {
 		return $this->hasMany('App\Article');
 	}
+
+	private static $messages = [
+		// name messages
+		'name.required' => 'El nombre es requerido',
+		'name.alpha' => 'El nombre no puede contener números ni símbolos',
+		'name.max' => 'El nombre debe tener menos de :max caracteres',
+		'name.min' => 'El nombre debe tener al menos :min caracteres',
+
+	];
+
+
 
 }
