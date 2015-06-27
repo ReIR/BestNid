5c187b85d4f8000dcd0944a464f5ef8ba11dc92c
Bruno Cascio <brunocascio@gmail.com>
2015-06-26 22:18:52 -0300

    fix countdown

--- src/public/js/article.countdown.js
+++ src/public/js/article.countdown.js
@@ -2,14 +2,19 @@ jQuery(function($){
   var end = $("#endDateIn").html();
   var endDate = new Date(end);
   var tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  var today = new Date();
   if (endDate > tomorrow) {
     $("#endTime").countdown(end, function(event) {
       $(this).text(
-        event.strftime('%D días')
+        event.strftime('%-D días')
       );
     });
   } else {
-    $("#endTime").html("Mañana");
+    if ( (endDate - 1) == today ) {
+      $("#endTime").html("Mañana");
+    } else {
+      $("#endTime").html("Ya venció");
+    }
   }
   $("#endDate").html(endDate.toLocaleDateString());
 });
