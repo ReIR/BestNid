f5aaf01805dd3056062e31ebcb97989cf82f4293
Cristian Daniel Barbaro <cristiandanielbarbaro@gmail.com>
2015-07-16 12:49:02 -0300

    fixed date in Article when show countdown.

--- src/public/js/article.countdown.js
+++ src/public/js/article.countdown.js
@@ -1,20 +1,21 @@
 jQuery(function($){
   var end = $("#endDateIn").html();
   var endDate = new Date(end);
+  var endDateUTC = new Date(endDate.getTime() + endDate.getTimezoneOffset() * 60000);
   var tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
   var today = new Date();
-  if (endDate > tomorrow) {
+  if (endDateUTC > tomorrow) {
     $("#endTime").countdown(end, function(event) {
       $(this).text(
         event.strftime('%-D días')
       );
     });
   } else {
-    if ( (endDate - 1) == today ) {
+    if ( new Date(endDateUTC - today).getHours() < 24) {
       $("#endTime").html("Mañana");
     } else {
       $("#endTime").html("Ya venció");
     }
   }
-  $("#endDate").html(endDate.toLocaleDateString());
+  $("#endDate").html(endDateUTC.toLocaleDateString());
 });
